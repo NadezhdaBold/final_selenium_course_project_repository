caadda0664ef94f856ea4ed4aa53ca9558afb59e
NadezhdaBold <snadyshka@gmail.com>
2024-11-05 18:39:56 +0300

    добавление метода-проверки и теста для ссылки login_link добавление неявного ожидания и перехвата исключений

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,11 +2,21 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 
 class BasePage():
-    def __init__(self, browser, url):  # конструктор в качестве параметров передаем экземпляр драйвера и url адрес
+    def __init__(self, browser, url, timeout=10):  # конструктор в качестве параметров передаем экземпляр драйвера и url адрес
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout) # добавим команду для неявного ожидания со значением по умолчанию в 10
+
 
     def open(self): # открыть ссылку в браузере
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
--- pages/main_page.py
+++ pages/main_page.py
@@ -8,3 +8,6 @@ class MainPage(BasePage): # добавляем класс MainPage, которы
     def go_to_login_page(self): # (self) чтобы иметь доступ к атрибутам и методам класса
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # Так как браузер хранится как аргумент класса BasePage, обращаться к нему нужно с помощью self
         login_link.click()
+
+    def should_be_login_link(self): # метод, проверяющий наличие ссылки
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), "Login link is not presented"
