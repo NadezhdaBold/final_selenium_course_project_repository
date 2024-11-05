1d252b8f2a07dbd8b92f8d62c9380b6e94891395
NadezhdaBold <snadyshka@gmail.com>
2024-11-05 17:34:09 +0300

    добавление класса MainPage в файл main_page и теста в test_main_page

---
+++ pages/main_page.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from  .base_page import BasePage
+
+class MainPage(BasePage): # добавляем класс MainPage, который наследует все методы и атрибуты предка BasePage
+    def go_to_login_page(self): # (self) чтобы иметь доступ к атрибутам и методам класса
+        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link") # Так как браузер хранится как аргумент класса BasePage, обращаться к нему нужно с помощью self
+        login_link.click()
